ef7cc734ae37611b11782eebe494e4a15a513da5
austin <austin.cawley@gmail.com>
2017-03-11 13:29:51 -0500

    updated build submission to allow name changes

--- build-submission.sh
+++ build-submission.sh
@@ -4,6 +4,12 @@
 # Exclude caches, outputs, and complied files
 # Output is in results
 
-zip -r build-submission.zip \
+if [ $1 ]; then
+FILENAME="CAWLEY-EDWARDS_Austin_${1}.zip"
+else
+FILENAME="CAWLEY-EDWARDS_Austin.zip"
+fi
+
+zip -r ${FILENAME} \
 	setup.py requirements.txt README.md results edges \
 	-x *__pycache__* *out* *.pyc
